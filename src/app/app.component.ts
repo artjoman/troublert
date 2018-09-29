08bcb13c0d8e1e687b6143234928bb695ed089bb
Blomis <arturs.blomis@accenture.com>
2018-09-29 10:38:04 +0300

    bla

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,70 +9,57 @@ import { icon, latLng, marker, polyline, tileLayer } from 'leaflet';
 export class AppComponent {
   title = 'troublert';
 
-  // Define our base layers so we can reference them multiple times
-  streetMaps = tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    detectRetina: true,
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-  });
-  wMaps = tileLayer('http://maps.wikimedia.org/osm-intl/{z}/{x}/{y}.png', {
+  map = tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     detectRetina: true,
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
   });
 
-  // Marker for the top of Mt. Ranier
-  // summit = marker([46.8523, -121.7603], {
-  //   icon: icon({
-  //     iconSize: [25, 41],
-  //     iconAnchor: [13, 41],
-  //     iconUrl: 'leaflet/marker-icon.png',
-  //     shadowUrl: 'leaflet/marker-shadow.png'
-  //   })
-  // });
 
-  // // Marker for the parking lot at the base of Mt. Ranier trails
-  // paradise = marker([46.78465227596462, -121.74141269177198], {
-  //   icon: icon({
-  //     iconSize: [25, 41],
-  //     iconAnchor: [13, 41],
-  //     iconUrl: 'leaflet/marker-icon.png',
-  //     shadowUrl: 'leaflet/marker-shadow.png'
-  //   })
-  // });
+  circles = {
+    LV1000: { lat: 56.9473662, lng: 24.1199864, radius: 100 },
+    LV1001: { lat: 56.961892, lng: 24.1230324, radius: 1000 },
+    LV1002: { lat: 56.9307861, lng: 24.0666199, radius: 1000 },
+    LV1003: { lat: 56.9399729, lng: 24.1387314, radius: 1200 },
+    LV1004: { lat: 56.9218272, lng: 24.1104354, radius: 1200 },
+    LV1005: { lat: 56.9958331, lng: 24.1144727, radius: 1500 },
+    LV1006: { lat: 56.9880202, lng: 24.1964526, radius: 3000 },
+    LV1007: { lat: 56.9946275, lng: 24.0764101, radius: 1200 },
+    LV1009: { lat: 56.951978, lng: 24.1482538, radius: 1200 },
+    LV1010: { lat: 56.9589215, lng: 24.1046542, radius: 1200 },
+    LV1011: { lat: 56.9527388, lng: 24.1275531, radius: 1200 }
+  };
 
-  // // Path from paradise to summit - most points omitted from this example for brevity
-  // route = polyline([[46.78465227596462, -121.74141269177198],
-  // [46.80047278292477, -121.73470708541572],
-  // [46.815471360459924, -121.72521826811135],
-  // [46.8360239546746, -121.7323131300509],
-  // [46.844306448474526, -121.73327445052564],
-  // [46.84979408048093, -121.74325201660395],
-  // [46.853193528950214, -121.74823296256363],
-  // [46.85322881676257, -121.74843915738165],
-  // [46.85119913890958, -121.7519719619304],
-  // [46.85103829018772, -121.7542376741767],
-  // [46.85101557523012, -121.75431755371392],
-  // [46.85140013694763, -121.75727385096252],
-  // [46.8525277543813, -121.75995212048292],
-  // [46.85290292836726, -121.76049157977104],
-  // [46.8528160918504, -121.76042997278273]]);
-
-  // Layers control object with our two base layers and the three overlay layers
+  // // Layers control object with our two base layers and the three overlay layers
   layersControl = {
     baseLayers: {
-      'Street Maps': this.streetMaps,
-      'Wikimedia Maps': this.wMaps
+      'Map': this.map
     },
-    // overlays: {
-    //   'Mt. Rainier Summit': this.summit,
-    //   'Mt. Rainier Paradise Start': this.paradise,
-    //   'Mt. Rainier Climb Route': this.route
-    // }
+    overlays: {
+      // 'Circles': this.circles;
+    }
   };
 
   // Set the initial set of displayed layers (we could also use the leafletLayers input binding for this)
   options = {
-    layers: [this.streetMaps],
+    layers: [this.map],
     zoom: 8,
     center: latLng([56.879966, 24.726909])
   };
+
+
+  loopCircleData() {
+    for (const key in this.circles) {
+      if (this.circles.hasOwnProperty(key)) {
+        let value = this.circles[key];
+        console.log(key, '::', value.radius);
+      }
+    }
+  }
+
+
+
+
+
+
+
 }
